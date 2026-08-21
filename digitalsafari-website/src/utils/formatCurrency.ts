3fd53ee96f8fa740212353d127fbb750d2fe6296
John Mwangi <sallywanjiru860@gmail.com>
2026-08-21 18:06:01 +0300

    folder update

--- digitalsafari-website/src/utils/formatCurrency.ts
+++
@@ -1,6 +0,0 @@
-export const formatCurrency = (amount: number, currency = 'KES') => {
-    return new Intl.NumberFormat('en-KE', {
-        style: 'currency',
-        currency,
-    }).format(amount);
-};
